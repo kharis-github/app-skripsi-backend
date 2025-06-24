21599c536c0c2ac5afb1c9de3842b95fe04e0064
unknown <kharis.amazio@gmail.com>
2025-06-24 11:21:52 +0700

    text preprocessing added

---
+++ app/classification/text.py
@@ -0,0 +1,92 @@
+import re
+from Sastrawi.StopWordRemover.StopWordRemoverFactory import StopWordRemoverFactory, StopWordRemover, ArrayDictionary
+from Sastrawi.Stemmer.StemmerFactory import StemmerFactory
+import requests
+import ast
+import pandas as pd
+
+# inisialisasi stopword remover
+stop_words = StopWordRemoverFactory().get_stop_words()
+
+new_array = ArrayDictionary(stop_words)
+stop_words_remover_new = StopWordRemover(new_array)
+
+# inisialisasi stemmer
+stemmer_factory = StemmerFactory()
+stemmer = stemmer_factory.create_stemmer()
+
+# fetch dictionary slang words Bahasa Indonesia dari github
+def get_slang_dict():
+    url = "https://raw.githubusercontent.com/louisowen6/NLP_bahasa_resources/master/combined_slang_words.txt"
+    response = requests.get(url)
+    dict = ast.literal_eval(response.text)
+    slang_dict = {f" {k} ": f" {v} " for k, v in dict.items()}
+    return slang_dict
+
+
+# text preprocessing: proses standar text preprocessing
+
+
+def text_cleaning(text):
+    text = re.sub(r'@[A-Za-z0-9_]+', '', text)  # hapus mentions
+    text = re.sub(r'#\w+', '', text)  # hapus tagar
+    text = re.sub(r'RT[\s]+', '', text)  # hapus retweet
+    text = re.sub(r'https?://\S+', '', text)  # hapus URL
+
+    text = re.sub(r'[^a-zA-Z\s]', '', text)  # hapus karakter non-alfanumerik
+    text = re.sub(r'\s+', ' ', text).strip()  # hapus spasi berlebihan
+
+    return text.lower() # lowercase
+
+# text normalization: normalisasi teks ke bentuk yang standar
+
+
+def normalisasi(str_text, norm):
+    for i in norm:
+        str_text = str_text.replace(i, norm[i])
+    return str_text
+
+
+# stopwords removal: hapus stopwords
+
+
+def stopwords_removal(text):
+
+    text = stop_words_remover_new.remove(text)
+    return text
+
+# tokenization
+
+
+def tokenize(text):
+    return text.split()
+
+
+# stemming untuk data yang reguler
+def stemming(text):
+    return stemmer.stem(text)
+
+# stemming untuk data yang telah tertokenisasi
+def stemming_for_tokenized(text_array):
+    stemmed_array = []
+    # stem setiap kata pada array
+    for w in text_array:
+        w_stemmed = stemmer.stem(w)
+        stemmed_array.append(w_stemmed)
+    # gabungkan stemmed_array menjadi satu string
+    stemmed_string = []
+    stemmed_string = " ".join(stemmed_array)
+    return stemmed_string
+
+async def text_preprocessing(data: pd.DataFrame):
+    # 1 | text cleaning
+    data['cleaning'] = data['full_text'].apply(text_cleaning)
+    # 2 | normalization
+    slang_dict = get_slang_dict()
+    data['normalized'] = data['cleaning'].apply(lambda x: normalisasi(x, slang_dict))
+    # 3 | stopwords removal
+    data['stopwords'] = data['normalized'].apply(stopwords_removal)
+    # 4 | stemming
+    data['stemming'] = data['stopwords'].apply(stemming)
+    # 5 | return
+    return data # kembalikan hasil bersih data, dan hasil proses-proses lainnya
--- app/routers/text.py
+++ app/routers/text.py
@@ -1,14 +1,26 @@
 from fastapi import APIRouter, Depends, HTTPException, UploadFile, File
 from sqlalchemy.orm import Session
 from app.database import SessionLocal
-from app.models.text import LabeledText
+from app.models.text import LabeledText, RawText
 from app.schemas.text import TextCreate, TextRead
 from app.crud.text import insert_rows_from_dataframe
+from pathlib import Path
 import pandas as pd
 import pyodbc
 import os
+import joblib
+from app.classification.text import text_cleaning, normalisasi, stopwords_removal, tokenize, stemming, get_slang_dict, text_preprocessing
 
 router = APIRouter(prefix="/text", tags=["Text"])
+
+# konfigurasi direktori
+current_dir = Path(__file__).resolve().parent
+nb_model_path = current_dir.parent / "classification" / "nb_model.joblib"
+svm_model_path = current_dir.parent / "classification" / "svm_model.joblib"
+
+# load model NB dan SVM
+nb_model = joblib.load(nb_model_path)  # naive bayes
+svm_model = joblib.load(svm_model_path)  # support vector machine
 
 
 def get_db():
@@ -56,3 +68,55 @@ async def upload_excel(file: UploadFile = File(...), db: Session = Depends(get_d
 
     # send json confirm sukses proses
     return {"status": "success"}
+
+# get raw data
+
+
+@router.get("/get")
+def list_texts(db: Session = Depends(get_db)):
+    text = db.query(RawText).all()
+    # print(f"[DEBUG] Data text = {text}")
+    return text
+
+# klasifikasi sekumpulan teks. digunakan untuk proses simulasi penelitian
+
+
+@router.post("/predict/batch")
+async def predict_batch(file: UploadFile = File(...), db: Session = Depends(get_db)):
+    # 1 | buka file excel upload, dan simpan di dalam pandas dataframe
+    contents = await file.read()
+    temp_path = "temp.xlsx"
+    with open(temp_path, "wb") as f:
+        f.write(contents)
+
+    df = pd.read_excel(temp_path)
+    df = df[['full_text', 'label']]
+
+    # # 2 | aplikasi text preprocessing (data cleaning, stopwords removal, stemming)
+    # slang_dict = get_slang_dict() # ambil dictionary slang words bahasa indonesia utk proses normalisasi
+    # df['cleaned_text'] = df['full_text'].apply(text_cleaning) # 2.a | text cleaning
+    # df['normalized'] = df['cleaned_text'].apply(lambda x: normalisasi(x, slang_dict)) # 2.b | normalisasi
+    # df['stopwords_removed'] = df['normalized'].apply(stopwords_removal) # 2.3 | stopwords removal
+    # df['stemmed'] = df['stopwords_removed'].apply(stemming) # 2.4 | stemming
+
+    # gunakan fungsi text_preprocessing untuk memproses data untuk di-klasifikasi
+    df = await text_preprocessing(df)
+
+    # 3 | Vectorization & Classification
+    # 3.a | Naive Bayes
+    nb_results = nb_model.predict(df['stemming'])
+    # 3.b | Support Vector Machine
+    svm_results = svm_model.predict(df['stemming'])
+
+    # 4 | Model Evaluation
+
+    os.remove(temp_path)  # hapus data temp file excel upload
+
+    return {
+        "nb_classification": nb_results,  # hasil klasifikasi NB
+        "svm_classification": svm_results,  # hasil klasifikasi SVM
+        "cleaning": df['cleaning'],  # sample data cleaning
+        "normalized": df['normalized'],  # sample data normalized
+        "stopwords": df['stopwords'],  # sample data stopwords
+        "stemming": df['stemming'],  # sample data stemming
+    }
